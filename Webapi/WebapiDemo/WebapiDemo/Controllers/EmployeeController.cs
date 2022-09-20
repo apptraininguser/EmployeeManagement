21259a1bb7bafd15f4cdb28752b9d97c194cad28
Sarath Baiju <sarath.bu@frontiersin.net>
2022-09-20 18:43:18 +0530

    Webapi Project added - Initial commit

---
+++ Webapi/WebapiDemo/WebapiDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebapiDemo.Repository;
+
+namespace WebapiDemo.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        //GetAllEmployee()
+        //api/employees
+        [HttpGet]
+        [Route("employees")]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var employeeRepository = new EmployeeRepository();
+                var employees = employeeRepository.GetAllEmployee();
+                return Ok(employees);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+           
+        }
+    }
+}
---
+++ Webapi/WebapiDemo/WebapiDemo/Repository/EmployeeRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebapiDemo.Repository
+{
+    public class EmployeeRepository
+    {
+        private SqlConnection _sqlConnection;
+
+        public EmployeeRepository()
+        {
+            _sqlConnection = new SqlConnection("data source= (localdb)\\mssqllocaldb; database= Training2022Dbfgg;");
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            try
+            {
+                _sqlConnection.Open();
+
+                var sqlCommand = new SqlCommand(cmdText: "select * from Employee",
+                    _sqlConnection);
+
+                var sqlDataReader = sqlCommand.ExecuteReader();
+
+                var listOfEmployee = new List<Employee>();
+
+                while (sqlDataReader.Read())
+                {
+                    listOfEmployee.Add(new Employee()
+                    {
+                        Id = (int)sqlDataReader["Id"],
+                        Name = (string)sqlDataReader["Name"],
+                        Age = (int)sqlDataReader["Age"],
+                        Salary = (int)sqlDataReader["Salary"]
+                    });
+                }
+                return listOfEmployee;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                _sqlConnection.Close();
+            }
+        }
+    }
+}
